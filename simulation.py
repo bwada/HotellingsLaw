3e9ed59d17ad68ae47adbed6d92266e97ca80f34
Brandon <bwada@hmc.edu>
2022-09-26 02:25:07 -0700

    position updates are largely in place need to make test case and verify it works

--- simulation.py
+++ simulation.py
@@ -1,11 +1,13 @@
-from re import I
 from typing import List
 import matplotlib.pyplot as plt
 import numpy as np
 from dataclasses import dataclass
 import random
-import matplotlib.animation as aniomation
+import matplotlib.animation as animation
 import copy
+
+SIM_WIDTH = 100
+SIM_HEIGHT = 10
 
 @dataclass
 class position:
@@ -18,19 +20,26 @@ class position:
     def vector(self, other_position):
         x_dir = other_position.x - self.x
         y_dir = other_position.y - self.y
-        vec = np.array(x_dir,y_dir)
+        vec = np.array((x_dir,y_dir))
         return vec/np.linalg.norm(vec)
 
-# @dataclass
-# class renderer:
-#     @staticmethod
-#     def render(vender_pos, people_pos):
-#         f,ax = plt.subplots()
-#         v_x = [pos.x for pos in vender_pos]
-#         v_y = [pos.y for pos in vender_pos]
-#         p_x = [pos.x for pos in people_pos]
-#         p_y = [pos.y for pos in people_pos]
-#         ax.scatter(p_x,p_y)
+    def move_to_target(self, goal, step_size):
+        if self.dist(goal)<step_size:
+            self.x = goal.x
+            self.y = goal.y
+            return True
+        v = self.vector(goal)
+        self.x += v[0]*step_size
+        self.y += v[1]*step_size
+
+    @staticmethod
+    def rand_dir():
+        xdir = random.random()-0.5
+        ydir = random.random()-0.5
+        vec = np.array((xdir,ydir))
+        norm = np.linalg.norm((xdir,ydir))
+        return(vec/norm)
+
 
 @dataclass
 class vendor:
@@ -39,29 +48,52 @@ class vendor:
     position_sales: int
     best_pos: position
     best_sales: int
+    wait_time: int
     moving: bool
     next_pos: position
 
     step_size = 0.5
+    max_wait_time = 20
+    explore_size = 4
 
     @classmethod
     def new_random_pos(cls, x_range, y_range):
         x = random.random()*(x_range[1]-x_range[0])+x_range[0]
         y = random.random()*(y_range[1]-y_range[0])+y_range[0]
-        return cls(position(x,y), 0, 0, position(x,y), 0, False, position(x,y))
+        return cls(position(x,y), 0, 0, position(x,y), 0, 0, False, position(x,y))
 
     def sale(self):
         self.position_sales += 1
         self.total_sales += 1
 
-    def move(self, goal):
-        if self.pos.dist(goal)<self.pos.dist(goal):
+    def pick_new_pos(self, pos, x_range=None, y_range=None):
+        new_dir = self.explore_size * position.rand_dir()
+        new_x = pos.x + new_dir[0]
+        new_y = pos.y + new_dir[1]
+        return position(new_x,new_y)
+
+    def timestep(self):
+        if self.moving:
+            self.move_to_target(self.next_pos)
+        else:
+            self.wait_time += 1
+            if self.wait_time >= self.max_wait_time:
+                if self.position_sales >= self.best_sales:
+                    self.best_sales = self.position_sales
+                    self.best_pos = copy.deepcopy(self.pos)
+                    new_pos = self.pick_new_pos(self.best_pos)
+                else:
+                    new_pos = self.pick_new_pos(self.best_pos)
+                self.moving = True
+                self.next_pos = new_pos
+    
+    def move_to_target(self, goal):
+        reached_goal = self.pos.move_to_target(goal, self.step_size)
+        if reached_goal:
             self.pos = copy.deepcopy(goal)
-        v = self.pos.vector(goal)
-        self.pos.x += v[0]*self.step_size
-        self.pos.y += v[1]*self.step_size
-
-
+            self.moving = False
+            self.wait_time = 0
+            self.position_sales = 0
 
 
 @dataclass
@@ -79,7 +111,8 @@ class person:
     def new_random_pos(cls, x_range, y_range):
         x = random.random()*(x_range[1]-x_range[0])+x_range[0]
         y = random.random()*(y_range[1]-y_range[0])+y_range[0]
-        return cls(position(x,y), False, cls.max_energy, None)
+        energy = random.random()*cls.max_energy
+        return cls(position(x,y), False, energy, None)
 
     def move(self,x_range=None,y_range=None):
         if self.hungry:
@@ -107,10 +140,9 @@ class person:
         self.update_pos(x_update,y_update,x_range=x_range,y_range=y_range)
     
     def move_to_target(self,x_range=None,y_range=None):
-        xdir = self.target.x-self.pos.x
-        ydir = self.target.y-self.pos.y
-        norm = np.linalg.norm((xdir,ydir))
-        self.update_pos(xdir*self.step_size/norm,ydir*self.step_size/norm,x_range=x_range,y_range=y_range)
+        reached_goal = self.pos.move_to_target(self.target,self.step_size)
+        if reached_goal:
+            self.eat()
     
     def update_pos(self,x_vec,y_vec,x_range,y_range):
         new_x = self.pos.x + x_vec
@@ -153,15 +185,13 @@ class environment:
         for ind,v in enumerate(self.vendors):
             dist = v.pos.dist(position)
             probs[ind] = np.exp(-dist)
-        print(position)
-        print(probs)
         probs = probs/np.sum(probs)
         vend = random.choices(self.vendors, weights=probs)[0]
         return vend 
 
     def timestep(self):
         for v in self.vendors:
-            pass
+            v.timestep()
         for p in self.people:
             p.move(x_range=(0,self.width),y_range=(0,self.height))
             if p.energy <= 0:
@@ -181,6 +211,7 @@ class environment:
         return x,y
     
 if __name__ == "__main__":
+    random.seed(1234)
     print("running")
     width = 100
     height = 10
@@ -199,5 +230,5 @@ if __name__ == "__main__":
         vendor_artist = ax.scatter(vendor_x,vendor_y,marker='x',color="red")
         return [people_artist,vendor_artist] 
     
-    ani = animation.FuncAnimation(fig, aniplot, np.arange(1, 100), interval=25, blit=True)
-    ani.save('animation.gif', writer='ffmpeg', fps=60)
+    ani = animation.FuncAnimation(fig, aniplot, np.arange(1, 200), interval=25, blit=True)
+    ani.save('animation.gif', writer='ffmpeg', fps=20)
